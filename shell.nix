8b4c0eba1660ee33bd30b91972caf284d7833f63
Elis Hirwing <elis@hirwing.se>
2018-08-14 18:15:45 +0200

    Improved nix tooling

--- shell.nix
+++ shell.nix
@@ -3,8 +3,19 @@ with (import <nixpkgs> {});
 mkShell {
   buildInputs = [
     # Tooling
-    emacs
+    (emacsWithPackages (epkgs: with epkgs; [
+      # buttercup # If added here cask won't install it and then it wont work
+      cask
+      cl-lib
+      request
+      undercover
+    ]))
+
+    # emacsPackages.buttercup # Needs to be added in nixpkgs so we can use the tool
     emacsPackages.cask
     gnumake
+
+    # Libraries
+    emacsPackagesNg.request
   ];
 }
